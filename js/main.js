78704a2cd74a280bd273b3eee1c0dfd4250aa4f8
Edgar Manuel Pérez Cob <59448174+edgarp9504@users.noreply.github.com>
2023-02-24 13:30:32 -0500

    se finalizo la página, falto detalles de estetica

--- js/main.js
+++ js/main.js
@@ -30,3 +30,129 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+const li        = document.querySelectorAll('.li')
+const bloque    = document.querySelectorAll('.bloque')
+
+// CLICK en li
+    // TODOS .li quitar la clase activo
+    // TODOS .bloque quitar la clase activo
+    // .li con la posicion se añadimos la clase activo
+    // .bloque con la posicion se añadimos la clase activo
+
+// Recorriendo todos los LI
+li.forEach( ( cadaLi , i )=>{
+    // Asignando un CLICK a CADALI
+    li[i].addEventListener('click',()=>{
+
+        // Recorrer TODOS los .li
+        li.forEach( ( cadaLi , i )=>{
+            // Quitando la clase activo de cada li
+            li[i].classList.remove('activo')
+            // Quitando la clase activo de cada bloque
+            bloque[i].classList.remove('activo')
+        })
+        console.log('aqui')
+        // En el li que hemos click le añadimos la clase activo
+        li[i].classList.add('activo')
+        // En el bloque con la misma posición le añadimos la clase activo
+        bloque[i].classList.add('activo')
+
+    })
+})
+
+
+// SELECTORES
+const selecContainer = document.getElementById('container__selector'),
+      selectorShow = document.getElementById('selector_show')
+
+let isSelectorOpen = false;
+
+if(selecContainer){
+    selecContainer.addEventListener('click', () =>{
+        isSelectorOpen = !isSelectorOpen;
+        if (isSelectorOpen) {
+            selectorShow.classList.add('open')
+        }  else {
+            selectorShow.classList.remove('open')
+        }
+    })
+}
+
+// SCHEDULE
+const selecSchedule = document.getElementById('container__schedule'),
+      scheduleShow = document.getElementById('selector_show_schedule')
+
+let isScheduleOpen = false;
+
+if(selecSchedule){
+    selecSchedule.addEventListener('click', () =>{
+        isScheduleOpen = !isScheduleOpen;
+        if (isScheduleOpen) {
+            scheduleShow.classList.add('open')
+        }  else {
+            scheduleShow.classList.remove('open')
+        }
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const scheduleLink = document.querySelectorAll('.nav__link')
+
+scheduleLink.forEach(n => n.addEventListener('click', () => {
+    isScheduleOpen = !isScheduleOpen;
+    scheduleShow.classList.remove('open')
+}))
+
+
+
+
+// lenguage
+const selecLenguage = document.getElementById('container__lenguage'),
+      lenguageShow = document.getElementById('selector_show_lenguaje')
+
+let isLenguageOpen = false;
+
+if(selecLenguage){
+    selecLenguage.addEventListener('click', () =>{
+        isLenguageOpen = !isLenguageOpen;
+        if (isLenguageOpen) {
+            lenguageShow.classList.add('open')
+        }  else {
+            lenguageShow.classList.remove('open')
+        }
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const lenguageLink = document.querySelectorAll('.nav__link')
+
+lenguageLink.forEach(n => n.addEventListener('click', () => {
+    isLenguageOpen = !isLenguageOpen;
+    lenguageShow.classList.remove('open')
+}))
+
+
+// card title__arrow
+const selectCard = document.getElementById('title__arrow'),
+      cardShow = document.getElementById('card__precio'),
+      arrowMov = document.getElementById('arr')
+
+let isCardeOpen = false;
+
+if(selectCard){
+    selectCard.addEventListener('click', () =>{
+        isCardeOpen = !isCardeOpen;
+        console.log('Hola mundo')
+        if (isCardeOpen) {
+            cardShow.classList.add('open_card')
+            arrowMov.classList.add('down')
+        }  else {
+            cardShow.classList.remove('open_card')
+            arrowMov.classList.remove('down')
+        }
+    })
+}
+
